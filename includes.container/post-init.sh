a2237aa19b8e6c1b15a55544f62166cb93f624fd
Dominik Schrom <39279764+schromyk@users.noreply.github.com>
2024-08-12 15:57:27 +0200

    Update post-init.sh

--- includes.container/post-init.sh
+++ includes.container/post-init.sh
@@ -3,9 +3,6 @@
 # surfshark
 ln -sf /.system/opt/Surfshark/surfshark /usr/bin/surfshark
 
-rm /usr/share/applications/surfshark.desktop
-mv surfshark.desktop /usr/share/applications/
-
 # pcloud
 mv pcloud /opt
 chmod +x /opt/pcloud
