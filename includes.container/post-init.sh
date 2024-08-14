a98b1f26a8740209c6f349f1554d0c97d30ef535
Dominik Schrom <39279764+schromyk@users.noreply.github.com>
2024-08-14 13:04:59 +0200

    Update post-init.sh

--- includes.container/post-init.sh
+++ includes.container/post-init.sh
@@ -8,24 +8,8 @@ chmod 755 /etc/rc.local
 ln -sf /.system/opt/Surfshark/surfshark /usr/bin/surfshark
 
 # pcloud
-mv pcloud /opt
-chmod +x /opt/pcloud
+#mv pcloud /opt
+#chmod +x /opt/pcloud
 
-ln -sf /.system/opt/pcloud /usr/bin/pcloud
-
-# dynamic backgrounds
-apt-get install -y git
-git clone https://github.com/saint-13/Linux_Dynamic_Wallpapers.git  
-cd Linux_Dynamic_Wallpapers
-
-if [[ -d /usr/share/backgrounds/Dynamic_Wallpapers ]]
-then 
-	rm -r /usr/share/backgrounds/Dynamic_Wallpapers
-fi
-
-cp -r ./Dynamic_Wallpapers/ /usr/share/backgrounds/
-cp ./xml/* /usr/share/gnome-background-properties/
-
-cd ..
-rm -r Linux_Dynamic_Wallpapers
-apt-get remove -y git
+#ln -sf /.system/opt/pcloud /usr/bin/pcloud
+rm pcloud
