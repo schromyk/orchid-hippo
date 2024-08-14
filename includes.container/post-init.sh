56b20f459365e98e7de080c0d06b414a5e6940c3
Dominik Schrom <39279764+schromyk@users.noreply.github.com>
2024-08-14 14:04:13 +0200

    Update post-init.sh

--- includes.container/post-init.sh
+++ includes.container/post-init.sh
@@ -12,3 +12,10 @@ mv pcloud /opt
 chmod +x /opt/pcloud
 
 ln -sf /.system/opt/pcloud /usr/bin/pcloud
+
+# gdm settings
+wget https://github.com/gdm-settings/gdm-settings/releases/latest/download/GDM_Settings.AppImage -O GDM_Settings.AppImage
+mv GDM_Settings.AppImage /opt
+chmod +x /opt/GDM_Settings.AppImage
+
+ln -sf /.system/opt/GDM_Settings.AppImage /usr/bin/gdm-settings
